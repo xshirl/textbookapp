e20c7185aac64d927ae5c89a6532c2d213bad70e
Shirley Xu <xshirl525@gmail.com>
2018-04-23 14:22:34 -0400

    add heroku db path to config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
   host: 'localhost',
   port: 5432,
-  database: 'DATABASE_URL'
+  database: 'postgres://jvzzwzmpdvdabh:9a49d3296fe86d4236469fecc13f9916232dc7407191f081292c84b195235ed0@ec2-23-23-247-222.compute-1.amazonaws.com:5432/dbkm4v7qbjcdk1'
 }
